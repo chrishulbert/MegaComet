9d111816c3774df898daad1bd7d508dbe1d0c82a
Chris Hulbert <chris.hulbert@gmail.com>
2011-08-01 22:47:57 +1000

    Figuring out how to spread out the test load

--- testing/megatest.c
+++ testing/megatest.c
@@ -23,17 +23,33 @@ struct ev_loop *libEvLoop; // The main libev loop. Global so that we don't have 
 ev_io conn[TEST_CONNS];
 int conns = 0;
 
-int findWorker(char* id) {
-	khint_t hash = kh_str_hash_func(id); // Do a crypto hash on the client
+int findWorker(char* clientIdStr) {
+	khint_t hash = kh_str_hash_func(clientIdStr); // Do a crypto hash on the client
 	int worker = hash % WORKERS; // Use the hash value to determine which worker they'll be on
+}
+
+// Find 64k id's that all point to the specified worker
+void runTestsAgainstWorker(int worker) {
+	printf("Running tests against worker %d\n", worker);
+	int clientId=0, clientsForWorker=0;
+	char clientIdStr[20];
+	while (clientsForWorker < TEST_CONNS) {
+		snprintf(clientIdStr,20,"%d",clientId);
+		if (findWorker(clientIdStr)==worker) {
+			clientsForWorker++;
+		}
+		clientId++;
+	}
+	printf("It took up to client id %d to fill this worker\n", clientId);
 }
 
 int main(int argc, char **args) {
 	if (argc<2) {
 		puts("MegaComet Tester");
 		puts("Usage: megatest N");
-		puts("Where N is the number of the client you wish to test");
+		printf("Where N is the number of the client you wish to test: 0-%d inclusive\n", WORKERS-1);
 		return 1;
 	}
+	runTestsAgainstWorker(atoi(args[1]));
 	return 0;
 }
